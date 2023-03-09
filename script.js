bc65abe84899debdddbb194f22b493c22d3787d9
Jackraheem <105651774+Jackraheem@users.noreply.github.com>
2023-03-09 15:50:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+function onaction(element){
+    console.log(element)
+
+}
+
+function resetform(){
+    alert("if you press ok all data will be deleted");
+}
+
+function validate(){
+
+    
+    }
+function selectem(val){
+    alert("select event fired");
+    console.log(val);
+}
+function oninf(){
+    alert("enter your  first name");
+}
+function oninl(){
+    alert("enter your last name");
+}
+function oninp(){
+    alert("enter your phone number");
+}
